86c86b8ae10ef0f0898c0032ca8d5e1a69a0bf61
Kyle Durand <kyledurand@gmail.com>
2021-12-18 17:24:13 -0500

    Stuff

--- deploy.sh
+++ deploy.sh
@@ -20,9 +20,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:kyledurand/kyledurand.github.io.git main
+# git push -f git@github.com:kyledurand/kyledurand.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:kyledurand/kyledurand.git main:gh-pages
+git push -f git@github.com:kyledurand/kyledurand.git main:gh-pages
 # git push -f git@github.com:kyledurand/kyledurand.git main
 cd -

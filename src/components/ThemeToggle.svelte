071c81a38b3c3a6f79b1de9e9f31d66542be4521
Kyle Durand <kyledurand@Kyles-MacBook-Air.local>
2021-12-18 16:44:02 -0500

    Initial commit

---
+++ src/components/ThemeToggle.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    if (import.meta.env.SSR) return undefined;
+
+    const root = document.documentElement;
+
+    const theme = window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "theme-dark"
+      : "theme-light";
+
+    root.classList = [];
+    root.classList.add(theme);
+  });
+
+  function setTheme(theme) {
+    const root = document.documentElement;
+
+    root.classList = [];
+    root.classList.add(theme);
+  }
+</script>
+
+<div>
+  <button on:click={() => setTheme("theme-dark")}>dark</button>
+  <button on:click={() => setTheme("theme-light")}>light</button>
+</div>

f20885fed346c0802be225a6b64acbede666c838
Kyle Durand <kyledurand@gmail.com>
2021-12-23 01:48:50 -0500

    bap bap

--- src/components/Parachute.svelte
+++ src/components/Parachute.svelte
@@ -1,31 +1,42 @@
 <script>
-  import { onDestroy } from "svelte";
+  import { onMount } from "svelte";
   import { draw } from "svelte/transition";
 
-  const States = {
-    falling: "falling",
-    open: "open",
-    down: "down",
-  };
+  const defaultPosition = "-74px";
+  const duration = 400;
 
-  let state = States.falling;
-  let landed = false;
+  // falling 100ms
+  // deployed 80ms
+  // landed `sum`
 
-  setTimeout(() => {
-    state = States.open;
-  }, 200);
+  let deployed;
+  let landed;
+  let position = defaultPosition;
 
-  setTimeout(() => {
-    state = States.down;
-    landed = true;
-  }, 1000);
-
-  onDestroy(() => {
-    state = undefined;
+  onMount(() => {
+    console.log("pre deploy");
+    position = "0px";
+    deploy();
   });
+
+  function deploy() {
+    setTimeout(() => {
+      console.log("pre land");
+      deployed = true;
+      position = "40px";
+      land();
+    }, duration);
+  }
+
+  function land() {
+    setTimeout(() => {
+      landed = true;
+      // console.log("land and rotate");
+    }, duration);
+  }
 </script>
 
-{#if state === States.down}
+{#if landed}
   <svg
     width="44"
     height="50"
@@ -40,11 +51,11 @@
       stroke-linecap="round"
     >
       <path
-        in:draw={{ duration: 800 }}
+        in:draw={{ duration }}
         d="M4 19.44C4 10.913 12.019 4 21.91 4c9.892 0 17.91 6.913 17.91 15.44L21.91 34.524 4 19.44Z"
         stroke-linejoin="round"
       />
-      <path in:draw={{ duration: 800 }} d="M4.045 45.95h35.91" />
+      <path in:draw={{ duration }} d="M4.045 45.95h35.91" />
     </g>
   </svg>
 {:else}
@@ -56,18 +67,19 @@
       fill-rule="evenodd"
       stroke-linecap="round"
     >
-      {#if state === States.open}
+      {#if deployed}
         <path
           d="M4.442 19.159c0-8.528 8.019-15.44 17.91-15.44 9.892 0 17.911 6.912 17.911 15.44l-17.91 15.083-17.91-15.083Z"
           stroke-linejoin="round"
-          class="parachute {state}"
+          class="parachute"
+          style="transform: translateY({position}); transition-duration: {duration}"
         />
       {:else}
         <path
-          in:draw={{ duration: 800 }}
           d="M13 7.44C15.785 5.147 18.755 4 21.91 4c3.155 0 6.126 1.147 8.91 3.44l-8.91 27.084L13 7.44Z"
           stroke-linejoin="round"
-          class="parachute {state}"
+          class="parachute"
+          style="transform: translateY({position}); transition-duration: {duration}"
         />
       {/if}
       <path d="M4.045 111.95h35.91" />
@@ -82,16 +94,7 @@
   }
 
   .parachute {
-    transform: translateY(-74px);
-    transition: transform 200ms ease-in-out;
-  }
-
-  .falling {
-    transform: translateY(0);
-  }
-
-  .open {
-    transform: translateY(40px);
+    transition: transform 300ms ease-in-out;
   }
 
   .landed {
--- src/components/ThemeToggle.svelte
+++ src/components/ThemeToggle.svelte
@@ -1,28 +1,43 @@
 <script>
   import { onMount } from "svelte";
 
+  let theme = "theme-light";
+
   onMount(() => {
-    if (import.meta.env.SSR) return undefined;
+    if (import.meta.env.SSR) return;
 
     const root = document.documentElement;
 
-    const theme = window.matchMedia("(prefers-color-scheme: dark)").matches
+    const preference = window.matchMedia("(prefers-color-scheme: dark)").matches
       ? "theme-dark"
       : "theme-light";
 
+    theme = preference;
     root.classList = [];
-    root.classList.add(theme);
+    root.classList.add(preference);
   });
 
-  function setTheme(theme) {
+  function setTheme(newTheme) {
     const root = document.documentElement;
-
+    theme = newTheme;
     root.classList = [];
     root.classList.add(theme);
   }
 </script>
 
 <div>
-  <button on:click={() => setTheme("theme-dark")}>dark</button>
-  <button on:click={() => setTheme("theme-light")}>light</button>
+  <button
+    type="button"
+    class={`plain-button ${theme === "theme-dark" && "active"}`}
+    on:click={() => setTheme("theme-dark")}
+  >
+    🌚
+  </button>
+  <button
+    type="button"
+    class={`plain-button ${theme === "theme-light" && "active"}`}
+    on:click={() => setTheme("theme-light")}
+  >
+    🌝
+  </button>
 </div>

51a7907c6aef67820c4cc72979588989c83b89ad
Kyle Durand <kyledurand@gmail.com>
2022-03-06 12:02:14 -0500

    Handle animation and reduced motion

--- src/components/Background.svelte
+++ src/components/Background.svelte
@@ -2,6 +2,10 @@
   import {onMount} from "svelte";
 
   onMount(() => {
+    let nodeDirections = [false, false];
+    const prefersReducedMotion = window.matchMedia(
+      "(prefers-reduced-motion: reduce)"
+    ).matches;
     const canvas = document.querySelector(".background");
     const context = canvas.getContext("2d");
     const width = window.innerWidth;
@@ -9,35 +13,39 @@
 
     let step = 0;
     let raf;
-    let line1Start = Math.floor(Math.random() * width);
-    let line1End = Math.floor(Math.random() * width);
+    let node1Start = Math.floor(Math.random() * width);
+    let node1End = Math.floor(Math.random() * width);
 
     canvas.height = height;
     canvas.width = width;
-    context.strokeStyle = "rgb(0 0 150  / 25%)";
+    context.strokeStyle = "rgb(0 0 150  / 15%)";
     context.lineWidth = 2;
     context.blur = 2;
     // Line 1 start
-    context.moveTo(line1Start, 0);
-    context.lineTo(line1End, height);
+    context.moveTo(node1Start, 0);
+    context.lineTo(node1End, height);
     context.stroke();
 
     const animation = setInterval(() => {
-      // Return if prefers reduced motion
+      if (prefersReducedMotion) return;
       raf = window.requestAnimationFrame(animate);
-    }, 50);
+    }, 1000 / 30);
 
     function animate() {
+      // Temp
       step++;
-      console.log({step});
+      // Check node directions
+      checkDirections();
 
-      line1Start++;
-      line1End++;
+      node1Start = nodeDirections[0] ? node1Start - 1 : node1Start + 1;
+      node1End = nodeDirections[1] ? node1End - 1 : node1End + 1;
+
+      console.log(nodeDirections);
       context.clearRect(0, 0, width, height);
       context.save();
       context.beginPath();
-      context.moveTo(line1Start, 0);
-      context.lineTo(line1End, height);
+      context.moveTo(node1Start, 0);
+      context.lineTo(node1End, height);
       context.stroke();
       context.restore();
 
@@ -49,10 +57,25 @@
 
       // IF REACHES 0 OR WIDTH, START MOVING UP OR DOWN
 
-      if (step >= 100) {
+      if (step >= 1000) {
+        console.log("end");
         clearInterval(animation);
         window.cancelAnimationFrame(raf);
       }
+    }
+
+    function checkDirections() {
+      nodeDirections[0] =
+        node1Start >= width ? !nodeDirections[0] : nodeDirections[0];
+
+      nodeDirections[1] =
+        node1End >= width ? !nodeDirections[1] : nodeDirections[1];
+
+      nodeDirections[0] =
+        node1Start >= 0 ? nodeDirections[0] : !nodeDirections[0];
+
+      nodeDirections[1] =
+        node1End >= 0 ? nodeDirections[1] : !nodeDirections[1];
     }
   });
 </script>

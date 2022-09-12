6ee0e30ec9684fca64b25d1672d5bf8919a89447
Kyle Durand <kyledurand@gmail.com>
2022-09-11 23:58:13 -0400

    extract function

--- src/components/Slides.svelte
+++ src/components/Slides.svelte
@@ -1,18 +1,18 @@
 <script>
   import {onMount} from "svelte";
 
+  const followMouse = (event) => {
+    const {innerHeight, innerWidth} = window;
+    const mouseXpercentage = Math.round((event.pageX / innerWidth) * 100);
+    const mouseYpercentage = Math.round((event.pageY / innerHeight) * 100);
+
+    const sections = document.querySelectorAll("section");
+    const style = `radial-gradient(at ${mouseXpercentage}% ${mouseYpercentage}%, var(--gradient-start), var(--gradient-end))`;
+
+    sections.forEach((node) => (node.style.background = style));
+  };
+
   onMount(() => {
-    function followMouse(event) {
-      const {innerHeight, innerWidth} = window;
-      const mouseXpercentage = Math.round((event.pageX / innerWidth) * 100);
-      const mouseYpercentage = Math.round((event.pageY / innerHeight) * 100);
-
-      const sections = document.querySelectorAll("section");
-      const style = `radial-gradient(at ${mouseXpercentage}% ${mouseYpercentage}%, var(--gradient-start), var(--gradient-end))`;
-
-      sections.forEach((node) => (node.style.background = style));
-    }
-
     document.addEventListener("mousemove", followMouse);
 
     return () => {
@@ -22,28 +22,32 @@
 </script>
 
 <main>
-  <section>section</section>
-  <section>section</section>
-  <section>section</section>
-  <section>
-    <h2 class="perspective">section</h2>
-    <p>static</p>
-  </section>
-  <section>section</section>
-  <section>section</section>
-  <section>section</section>
-  <section>section</section>
-  <section>section</section>
-  <section>section</section>
-  <section>section</section>
-  <section>section</section>
-  <section>section</section>
+  <section>This</section>
+  <section>is</section>
+  <section>neat.</section>
+
+  <section><span class="perspective">ahahahahaha1</span></section>
+  <section><span class="perspective">ahahahahaha2</span></section>
+  <section><span class="perspective">ahahahahaha3</span></section>
+
+  <section><span class="perspective">whooaoaaaaa1</span></section>
+  <section><span class="perspective">whooaoaaaaa2</span></section>
+  <section><span class="perspective">whooaoaaaaa3</span></section>
+
+  <section><span class="perspective">weeeeoooooo1</span></section>
+  <section><span class="perspective">weeeeoooooo2</span></section>
+  <section><span class="perspective">weeeeoooooo3</span></section>
 </main>
 
 <style>
+  .perspective {
+    transform: translateZ(12px);
+  }
+
   main {
     --gradient-start: #2b2b2b;
     --gradient-end: #000000;
+    position: relative;
     display: grid;
     grid-template-columns: repeat(3, 1fr);
     width: 100vw;
@@ -56,6 +60,8 @@
   }
 
   section {
+    --scale: 3;
+    position: relative;
     display: grid;
     place-content: center;
     background-color: #eee;
@@ -74,7 +80,15 @@
     );
   }
 
-  .perspective {
-    transform: translateZ(3px);
+  section::after {
+    position: absolute;
+    font-size: 2rem;
+  }
+
+  section:not(:nth-child(3n + 3))::after {
+    content: ">";
+    right: 0;
+    top: 50%;
+    transform: translateY(-50%);
   }
 </style>
